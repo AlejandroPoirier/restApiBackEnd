7dca9beb61549219a1419e70b2f0cfaaf66f407b
Alejandro GPA <ale.poirier16@gmail.com>
2023-03-27 09:18:48 -0300

    2nd commit after some minor changes

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
+const feeedRoutes = require('./routes/feed')
+
 const app = express()
+
+//forward every request starting with '/feed' to the feedRoutes
+app.use('/feed',feeedRoutes);
 
 app.listen(8080);
 
