add237bfe13c8e5dffc13090565746faa4e8e766
Alejandro GPA <ale.poirier16@gmail.com>
2023-03-29 08:49:33 -0300

    Add createPost method for the rest api

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 
+const bodyParser = require('body-parser');
+
 const feeedRoutes = require('./routes/feed')
 
 const app = express()
+
+//app.use(bodyParser.urlencoded());   => for data  x-www-form-urlendoded.
+app.use(bodyParser.json()); //=> to parse data in JSON format
 
 //forward every request starting with '/feed' to the feedRoutes
 app.use('/feed',feeedRoutes);
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,29 @@
 exports.getPosts = (req, res, next) => {
-    
+    //while usign the REST arquitecture, we don´t return HTML,
+    //so we won´t be using res.render() method.
+    //instead we'll use res.json()
+
+    // we should assing the status code also
+    res.status(200).json({
+        posts: [{title:"FP", content: 'First post content'}]
+    });
+};
+
+
+exports.createPosts = (req,res, next) => {
+    const title = req.body.title;
+    const content = req.body.content;
+
+    //Create post in DB
+
+    // 200 - Success.
+    // 201 -Success, a resource was created.
+    res.status(201).json({
+        message: 'Post created successfully!',
+        post: {
+            id: new Date().toISOString(),
+            title: title,
+            post: content
+        }
+    });
 }
