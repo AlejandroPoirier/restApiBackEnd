73b07365cc389e30fe8cc4061ff0207461049777
Alejandro GPA <ale.poirier16@gmail.com>
2023-04-05 09:09:40 -0400

    Add validation middleware to Post requests

--- routes/feed.js
+++ routes/feed.js
@@ -1,11 +1,27 @@
 const express = require('express');
 
+// import the 'check' module and assings its body property
+//to a variable named body using destructuring ´{}´
+
+//{The curly braces in the syntax indicate object destructuring,
+//a feature introduced in ES6 that allows extracting properties from 
+//objects and binding them to variables with the same name.}
+
+//'body' is a function that performs validation checks on
+//request bodies  in an Express.js application
+const { body } = require('express-validator/check');
+
 const feedController = require('../controllers/feed');
 
 const router = express.Router();
 
+// GET /feed/posts
 router.get('/posts', feedController.getPosts);
 
-router.post('/post', feedController.createPosts);
+// POST /feed/post
+router.post('/post', [
+    body('title').trim().isLength({min: 5}),
+    body('content').trim().isLength({min: 5})
+],feedController.createPosts);
 
 module.exports = router;
